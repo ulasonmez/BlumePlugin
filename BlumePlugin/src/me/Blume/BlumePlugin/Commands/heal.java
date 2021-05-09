74714e7a489713752ae27be679b14789e265ed66
ulasonmez <ulasonmez@outlook.com>
2021-05-09 14:49:15 +0300

    heal added weather clear

--- BlumePlugin/src/me/Blume/BlumePlugin/Commands/heal.java
+++ BlumePlugin/src/me/Blume/BlumePlugin/Commands/heal.java
@@ -1,5 +1,6 @@
 package me.Blume.BlumePlugin.Commands;
 
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,6 +18,8 @@ public class heal implements CommandExecutor{
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(label.equals("heal") && sender instanceof Player) {
 			Player player = (Player) sender;
+			World world= player.getWorld();
+			world.setClearWeatherDuration(1000000000);
 			if(player.getHealth()==20.0 && player.getFoodLevel()==20) {
 				player.sendMessage(ChatColor.GREEN+"You are already full.");
 			return true;
